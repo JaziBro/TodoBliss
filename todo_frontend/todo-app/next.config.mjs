3a8a356205aff23cb35a72ce1cb73ad9ba675f7d
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2024-08-11 22:55:09 +0500

    Updated next.config.mjs

--- todo_frontend/todo-app/next.config.mjs
+++ todo_frontend/todo-app/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Ensure this is enabled for app directory usage
+    },
+    env: {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
+  };
+  
+  export default nextConfig;
+  
